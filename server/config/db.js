6adabad444f44540072153845d1ad27402c00d78
Priyanshu <mishrapriyanshu026@gmail.com>
2025-05-25 13:59:27 +0530

    database connected

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      `Conneted To Mongodb Atlas`.bgMagenta.white
     );
   } catch (error) {
     console.log(`Errro in Mongodb ${error}`.bgRed.white);
